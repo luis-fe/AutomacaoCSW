6533d3f5c4b5f417378c533b747085fbfc53fa0b
luis-fe <luis_engp@hotmail.com>
2023-05-27 10:59:04 -0300

    Novo

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ port = int(os.environ.get('PORT', 5000))
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 # Press the green button in the gutter to run the script.
