dc79e70f1196d250943dc7e0334651520e7a9e34
luis-fe <luis_engp@hotmail.com>
2023-05-28 12:58:26 -0300

    Verificar JDK

--- ConexaoJava.py
+++ ConexaoJava.py
@@ -1,11 +1,17 @@
-import platform
+import subprocess
 
-def check_jdk():
-    java_version = platform.java_ver()
-    if java_version:
-        return print("Versão do JDK encontrada:", java_version)
-    else:
-        return print("JDK não encontrado.")
+def check_java_version():
+    try:
+        result = subprocess.run(['java', '-version'], capture_output=True, text=True)
+        output = result.stderr.strip()
+        if result.returncode == 0 and 'java version' in output:
+            version_line = output.splitlines()[0]
+            java_version = version_line.split()[-1]
+            return print(f"JDK version: {java_version}")
+        else:
+            return print("JDK not found or command execution failed.")
+    except FileNotFoundError:
+        return print("Java executable not found.")
 
 
-
+check_java_version()
